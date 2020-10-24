7537b723d900acbe16bd04453d0ce6a2524a880d
Bala Luguja <bluguja@gmail.com>
2020-10-24 12:15:48 +0000

    Automatically backed up by Learn

---
+++ db/migrate/20201024115137_create_user.rb
@@ -0,0 +1,10 @@
+class CreateUser < ActiveRecord::Migration
+    def change
+       create_table :users 
+      t.string :name
+      t.string :email
+      t.string :password
+    end 
+  end
+  
+end
